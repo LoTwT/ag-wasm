792472003f68fa92b14cec4e5d8324e9d536d819
Lo <l709937065@gmail.com>
2024-04-20 23:35:42 +0800

    refactor: change to monorepo

---
+++ packages/ag-wasm/README.md
@@ -0,0 +1,9 @@
+# ag-wasm
+
+A forked version of the unpublished `ast-grep-wasm` in the [ast-grep.github.io](https://github.com/ast-grep/ast-grep.github.io).
+
+Please see [ag-wasm](https://github.com/lotwt/ag-wasm).
+
+## License
+
+[MIT](./LICENSE)
--- scripts/build.ts
+++ scripts/build.ts
@@ -1,21 +1,19 @@
 import { resolve } from "node:path"
 import { fileURLToPath } from "node:url"
+import { rimraf } from "rimraf"
 import { copy, ensureDir } from "fs-extra"
-import { type PackageJson, readPackageJSON, writePackageJSON } from "pkg-types"
 import fg from "fast-glob"
 
 const _dirname = fileURLToPath(new URL(".", import.meta.url))
 
-const sharedFiles = [
-  ["LICENSE", "LICENSE"],
-  ["README.md", "README.md"],
-]
-
 async function build() {
   console.log("\n🚀 Start Building...\n")
 
-  ensureDir("dist")
-  ensureDir("dist/dist")
+  const pkgPath = resolve(_dirname, "../packages/ag-wasm")
+  const distPath = resolve(pkgPath, "dist")
+
+  await rimraf(distPath)
+  await ensureDir(distPath)
 
   const pkgFiles = (
     await fg("pkg/**/*", {
@@ -28,82 +26,18 @@ async function build() {
       ],
     })
   )
-    .map((file) => [file, file.replace("pkg/", "dist/")])
+    .map((file) => [file, file.replace("pkg/", "")])
     .map(([_, to]) => [
       _,
-      to === "dist/node/index.js" || to === "dist/node/index.d.ts"
+      to === "node/index.js" || to === "node/index.d.ts"
         ? `${to.slice(0, -2)}c${to.slice(-2)}`
         : to,
     ])
 
-  const files = [...sharedFiles, ...pkgFiles]
-
   await Promise.all(
-    files.map(([from, to]) =>
-      copy(resolve(_dirname, "..", from), resolve(_dirname, "../dist", to)),
+    pkgFiles.map(([from, to]) =>
+      copy(resolve(_dirname, "..", from), resolve(distPath, to)),
     ),
-  )
-
-  const localPkgJson = await readPackageJSON()
-  const wasmPkgjSON = await readPackageJSON(resolve(_dirname, "../pkg/node"))
-
-  const publishPkgJson: PackageJson = {
-    name: localPkgJson.name,
-    description: localPkgJson.description,
-    type: "module",
-    author: localPkgJson.author,
-    license: localPkgJson.license,
-    homepage: localPkgJson.homepage,
-    repository: localPkgJson.repository,
-    bugs: localPkgJson.bugs,
-    keywords: localPkgJson.keywords,
-    files: ["dist"],
-    sideEffects: ["**/*.wasm"],
-    exports: {
-      ".": {
-        require: "./dist/web/index.js",
-        import: "./dist/web/index.js",
-      },
-      "./node": {
-        require: "./dist/node/index.cjs",
-        import: "./dist/node/index.cjs",
-      },
-      "./bundler": {
-        require: "./dist/bundler/index.js",
-        import: "./dist/bundler/index.js",
-      },
-      "./wasm": {
-        require: "./dist/web/index_bg.wasm",
-        import: "./dist/web/index_bg.wasm",
-      },
-      "./node/wasm": {
-        require: "./dist/node/index_bg.wasm",
-        import: "./dist/node/index_bg.wasm",
-      },
-      "./bundler/wasm": {
-        require: "./dist/bundler/index_bg.wasm",
-        import: "./dist/bundler/index_bg.wasm",
-      },
-      // @ts-ignore
-      "./*": ["./*", "./*.d.ts"],
-    },
-    main: "./dist/web/index.js",
-    module: "./dist/web/index.js",
-    types: "./dist/web/index.d.ts",
-    typesVersions: {
-      "*": {
-        "*": ["./dist/*", "./*"],
-      },
-    },
-    dependencies: wasmPkgjSON.dependencies,
-    scripts: {
-      release: "npm publish",
-    },
-  }
-
-  await writePackageJSON(
-    resolve(_dirname, "../dist/package.json"),
-    publishPkgJson,
   )
 
   console.log("\n📦 Finish Building...\n")
